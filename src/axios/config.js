655ae5edaa9ed552f2b922459fbdc177745de5f1
Klaus7067 <riansouza589@gmail.com>
2023-03-29 00:47:22 -0300

    update

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const blogFetch = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:3000",
-=======
-  baseURL: "https://rian.onrender.com",
->>>>>>> a905a44cceb00dfe5dce5795a23cc0cda8c1873f
+  baseURL: "https://receitas-back.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
